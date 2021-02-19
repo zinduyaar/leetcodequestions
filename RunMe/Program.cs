4aeb2e12dcb86fa968479c7fa0d2e6f0de251b77
amansinghgusain <amansinghgusain@gmail.com>
2021-02-19 16:53:41 +0530

    Initial commit

---
+++ RunMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
