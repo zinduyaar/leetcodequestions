8eff104a1111f254d8ec40686918e512adfc1262
amansinghgusain <amansinghgusain@gmail.com>
2021-02-19 17:39:19 +0530

    TwoSum

--- RunMe/Program.cs
+++ RunMe/Program.cs
@@ -6,7 +6,8 @@ namespace RunMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// 19th Feb 2021
+// https://leetcode.com/problems/two-sum/
+
+public class Solution
+{
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var combinator = target - nums[i];
+                var combinatorIndex = Array.FindIndex(nums, w => w == combinator);
+
+                if (combinatorIndex >= 0 && combinatorIndex != i)
+                {
+                    result[0] = i;
+                    result[1] = combinatorIndex;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
